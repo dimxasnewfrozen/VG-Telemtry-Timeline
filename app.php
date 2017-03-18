e51de6096f37554f044b7f121eb8f41765757b15
Kenny <dimxasnewfrozen@gmail.com>
2017-03-18 10:23:32 -0400

    try and fake heroku out.

---
+++ app.php
@@ -0,0 +1,6 @@
+<?php 
+// Deploying static files to heroku doesn't work
+// Heroku needs a buildpack/server so have heroku detect this file
+// create the buildpack and launch the app
+include_once("indclude.html"); 
+?>
