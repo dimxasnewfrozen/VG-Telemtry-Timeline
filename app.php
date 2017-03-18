b5572bdeb9c3ee7e9c95531ad0e863f1111f3775
Kenny <dimxasnewfrozen@gmail.com>
2017-03-18 10:27:53 -0400

    keep trying

--- app.php
+++ app.php
@@ -2,5 +2,5 @@
 // Deploying static files to heroku doesn't work
 // Heroku needs a buildpack/server so have heroku detect this file
 // create the buildpack and launch the app
-include_once("indclude.html"); 
+include_once("home.html"); 
 ?>
